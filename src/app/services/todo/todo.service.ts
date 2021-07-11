5ca2b516d4ebc1b9ba7c20fb264584223afe0232
puku0x <puku0x@gmail.com>
2021-07-11 18:46:40 +0900

    refactor(services): refactor todo service

--- src/app/services/todo/todo.service.ts
+++ src/app/services/todo/todo.service.ts
@@ -3,19 +3,14 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-import { Todo } from '../../models';
+import { Todo, TodoCreateDto, TodoUpdateDto } from '../../models';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
-  constructor(
-    private http: HttpClient,
-    @Inject('API_URL') private url: string
-  ) {}
-
   findAll(offset?: number, limit?: number): Observable<Todo[]> {
-    const url = `${this.url}/todos`;
+    const url = `${this.baseUrl}/todos`;
     let params = new HttpParams();
     params = offset ? params.set('offset', `${offset}`) : params;
     params = limit ? params.set('limit', `${limit}`) : params;
@@ -23,22 +18,27 @@ export class TodoService {
   }
 
   find(id: string): Observable<Todo> {
-    const url = `${this.url}/todos/${id}`;
+    const url = `${this.baseUrl}/todos/${id}`;
     return this.http.get<Todo>(url);
   }
 
-  create(todo: Partial<Todo>): Observable<Todo> {
-    const url = `${this.url}/todos`;
+  create(todo: TodoCreateDto): Observable<Todo> {
+    const url = `${this.baseUrl}/todos`;
     return this.http.post<Todo>(url, todo);
   }
 
-  update(todo: Partial<Todo>): Observable<Todo> {
-    const url = `${this.url}/todos/${todo.id}`;
+  update(todo: TodoUpdateDto): Observable<Todo> {
+    const url = `${this.baseUrl}/todos/${todo.id}`;
     return this.http.put<Todo>(url, todo);
   }
 
   remove(id: string): Observable<string> {
-    const url = `${this.url}/todos/${id}`;
+    const url = `${this.baseUrl}/todos/${id}`;
     return this.http.delete<void>(url).pipe(map(() => id));
   }
+
+  constructor(
+    private http: HttpClient,
+    @Inject('API_URL') private baseUrl: string
+  ) {}
 }
